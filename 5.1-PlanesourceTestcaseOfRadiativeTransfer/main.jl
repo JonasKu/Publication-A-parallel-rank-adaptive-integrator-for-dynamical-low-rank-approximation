c8278c0a5eb5d375946b2a044721d41031eb08be
JonasKu <jonas.kusch@rwth-aachen.de>
2023-10-13 11:17:14 +0200

    added comparison to PN

--- 5.1-PlanesourceTestcaseOfRadiativeTransfer/main.jl
+++ 5.1-PlanesourceTestcaseOfRadiativeTransfer/main.jl
@@ -6,42 +6,9 @@ using NPZ
 using PyPlot
 
 s = Settings();
-ϑCoarse = 2e-2;
 ϑFine = 1e-2;
 
-# run solver for different end times and different epsilons
-
-# end time tEnd = 2, ϑ = 1e-1
-s.ϑ = ϑCoarse;
-s.tEnd = 2.0;
-solver = Solver(s);
-@time tEnd, u3P, _ = SolveParallelRejection(solver);
-
-# end time tEnd = 2.75, ϑ = 1e-1
-s.tEnd = 2.75;
-solver = Solver(s);
-@time tEnd, u4P, _ = SolveParallelRejection(solver);
-
-# end time tEnd = 5, ϑ = 1e-1
-s.tEnd = 5.0;
-solver = Solver(s);
-@time tEnd, u5P, ranks, η, ηBound = SolveParallelRejection(solver);
-
-# end time tEnd = 2, ϑ = 1e-1
-s.tEnd = 2.0;
-solver = Solver(s);
-@time tEnd, u3, _ = SolveBUGrank_adapt_rejection(solver);
-
-# end time tEnd = 2.75, ϑ = 1e-1
-s.tEnd = 2.75;
-solver = Solver(s);
-@time tEnd, u4, _ = SolveBUGrank_adapt_rejection(solver);
-
-# end time tEnd = 5, ϑ = 1e-1
-s.tEnd = 5.0;
-solver = Solver(s);
-@time tEnd, u5, ranksBUG, ηBug, ηBugBound = SolveBUGrank_adapt_rejection(solver);
-
+# run solver for different end times
 # end time tEnd = 2, ϑ = 5e-2
 s.ϑ = ϑFine;
 s.tEnd = 2.0;
@@ -72,6 +39,12 @@ solver = Solver(s);
 s.tEnd = 5.0;
 solver = Solver(s);
 @time tEnd, u5F, ranksBUGF, ηBugF, ηBoundBugF = SolveBUGrank_adapt_rejection(solver);
+
+# full baseline
+# end time tEnd = 2.75
+s.tEnd = 2.75;
+solver = Solver(s);
+@time tEnd, uPN = Solve(solver);
 
 ## plotting ##
 
@@ -119,39 +92,6 @@ uEx3 = [uEx3[end:-1:2];uEx3];
 
 # start plot BUG
 fig, ax = subplots(figsize=(15, 12), dpi=100)
-ax.plot(s.xMid,u3[:,1], "b--", linewidth=2, label=L"$t=2$, BUG", alpha=1.0)
-ax.plot(x3,uEx3, "b-", linewidth=2, alpha=0.5)
-ax.plot(s.xMid,u4[:,1], "r:", linewidth=2, label=L"$t=2.75$, BUG", alpha=1.0)
-ax.plot(x4,uEx4, "r-", linewidth=2, alpha=0.5)
-ax.plot(s.xMid,u5[:,1], "g-.", linewidth=2, label=L"$t=5$, BUG", alpha=1.0)
-ax.plot(x5,uEx5, "g-", linewidth=2, alpha=0.5)
-ylabel(L"\Phi", fontsize=30)
-ax.set_xlim([-5,5])
-ax.set_xlabel("x", fontsize=30);
-ax.legend(loc="upper right", fontsize=30)
-ax.tick_params("both",labelsize=30) 
-fig.canvas.draw() # Update the figure
-PyPlot.savefig("results/PhiBUGThetaCoarse.png")
-
-
-# start plot P-BUG
-fig, ax = subplots(figsize=(15, 12), dpi=100)
-ax.plot(s.xMid,u3P[:,1], "b--", linewidth=2, label=L"$t=2$, parallel", alpha=1.0)
-ax.plot(x3,uEx3, "b-", linewidth=2, alpha=0.5)
-ax.plot(s.xMid,u4P[:,1], "r:", linewidth=2, label=L"$t=2.75$, parallel", alpha=1.0)
-ax.plot(x4,uEx4, "r-", linewidth=2, alpha=0.5)
-ax.plot(s.xMid,u5P[:,1], "g-.", linewidth=2, label=L"$t=5$, parallel", alpha=1.0)
-ax.plot(x5,uEx5, "g-", linewidth=2, alpha=0.5)
-ylabel(L"\Phi", fontsize=30)
-ax.set_xlim([-5,5])
-ax.set_xlabel("x", fontsize=30);
-ax.legend(loc="upper right", fontsize=30)
-ax.tick_params("both",labelsize=30) 
-fig.canvas.draw() # Update the figure
-PyPlot.savefig("results/PhiParallelThetaCoarse.png")
-
-# start plot BUG
-fig, ax = subplots(figsize=(15, 12), dpi=100)
 ax.plot(s.xMid,u3F[:,1], "b--", linewidth=2, label=L"$t=2$, BUG", alpha=1.0)
 ax.plot(x3,uEx3, "b-", linewidth=2, alpha=0.5)
 ax.plot(s.xMid,u4F[:,1], "r:", linewidth=2, label=L"$t=2.75$, BUG", alpha=1.0)
@@ -166,7 +106,6 @@ ax.tick_params("both",labelsize=30)
 fig.canvas.draw() # Update the figure
 PyPlot.savefig("results/PhiBUGThetaFine.png")
 
-
 # start plot P-BUG
 fig, ax = subplots(figsize=(15, 12), dpi=100)
 ax.plot(s.xMid,u3PF[:,1], "b--", linewidth=2, label=L"$t=2$, parallel", alpha=1.0)
@@ -175,6 +114,7 @@ ax.plot(s.xMid,u4PF[:,1], "r:", linewidth=2, label=L"$t=2.75$, parallel", alpha=
 ax.plot(x4,uEx4, "r-", linewidth=2, alpha=0.5)
 ax.plot(s.xMid,u5PF[:,1], "g-.", linewidth=2, label=L"$t=5$, parallel", alpha=1.0)
 ax.plot(x5,uEx5, "g-", linewidth=2, alpha=0.5)
+#ax.plot(s.xMid,uPN[:,1], "k-.", linewidth=2, label=L"PN", alpha=1.0)
 ylabel(L"\Phi", fontsize=30)
 ax.set_xlim([-5,5])
 ax.set_xlabel("x", fontsize=30);
@@ -183,33 +123,17 @@ ax.tick_params("both",labelsize=30)
 fig.canvas.draw() # Update the figure
 PyPlot.savefig("results/PhiParallelThetaFine.png")
 
-# plot rank in time
+# start plot BUG, Parallel, PN comparison
 fig, ax = subplots(figsize=(15, 12), dpi=100)
-ax.plot(ranks[1,:],ranks[2,:], "k-", linewidth=2, alpha=1.0)
-ax.plot([2,2],[0,100], "b--", linewidth=2, alpha=1.0)
-ax.plot([2.75,2.75],[0,100], "r:", linewidth=2, alpha=1.0)
-ax.plot([5,5],[0,100], "g-.", linewidth=2, alpha=1.0)
-ax.set_xlim([ranks[1,1],ranks[1,end]+0.05])
-ax.set_ylim([0,maximum(ranks[2,2:end])+2])
-ax.set_xlabel("time", fontsize=30);
-ax.set_ylabel("rank", fontsize=30);
-ax.tick_params("both",labelsize=30) 
+ax.plot(s.xMid,u4F[:,1] - uPN[:,1], "g-", linewidth=2, label=L"$t=2.75$, BUG", alpha=1.0)
+ax.plot(s.xMid,u4PF[:,1] - uPN[:,1], "b-.", linewidth=2, label=L"$t=2.75$, parallel", alpha=1.0)
+ax.set_xlim([-5,5])
+ax.set_xlabel("x", fontsize=22);
+ax.legend(loc="upper right", fontsize=22)
+ax.tick_params("both",labelsize=22) 
+tight_layout()
 fig.canvas.draw() # Update the figure
-PyPlot.savefig("results/ranksParallelThetaCoarse.png")
-
-# plot rank in time
-fig, ax = subplots(figsize=(15, 12), dpi=100)
-ax.plot(ranksBUG[1,:],ranksBUG[2,:], "k-", linewidth=2, alpha=1.0)
-ax.plot([2,2],[0,100], "b--", linewidth=2, alpha=1.0)
-ax.plot([2.75,2.75],[0,100], "r:", linewidth=2, alpha=1.0)
-ax.plot([5,5],[0,100], "g-.", linewidth=2, alpha=1.0)
-ax.set_xlim([ranksBUG[1,1],ranksBUG[1,end]+0.05])
-ax.set_ylim([0,max(maximum(ranks[2,2:end]),maximum(ranksBUG[2,2:end]))+2])
-ax.set_xlabel("time", fontsize=30);
-ax.set_ylabel("rank", fontsize=30);
-ax.tick_params("both",labelsize=30) 
-fig.canvas.draw() # Update the figure
-PyPlot.savefig("results/ranksBUGThetaCoarse.png")
+PyPlot.savefig("results/PhiDistanceToPN.png")
 
 # plot rank in time
 fig, ax = subplots(figsize=(15, 12), dpi=100)
@@ -240,22 +164,6 @@ fig.canvas.draw() # Update the figure
 PyPlot.savefig("results/ranksBUGThetaFine.png")
 
 # plot η in time
-fig = figure("eta coarse",figsize=(15,12),dpi=100)
-ax = gca()
-ax.plot(ηBug[:,1],ηBug[:,2], "r-.", label=L"BUG, $\bar\vartheta =$ "*LaTeXString(string(ϑCoarse)), linewidth=2, alpha=1.0)
-ax.plot(η[:,1],η[:,2], "b--", label=L"parallel, $\bar\vartheta =$ "*LaTeXString(string(ϑCoarse)), linewidth=2, alpha=1.0)
-ax.plot(ηBugBound[:,1],ηBugBound[:,2], "r:", label=L"$c\Vert\psi\Vert\bar\vartheta_{\mathrm{BUG}} / h$, $c = $ "*LaTeXString(string(s.cη)), linewidth=2, alpha=1.0)
-ax.plot(ηBound[:,1],ηBound[:,2], "b-", label=L"$c\Vert\psi\Vert\bar\vartheta_{\mathrm{parallel}} / h$, $c = $ "*LaTeXString(string(s.cη)), linewidth=2, alpha=0.4)
-ax.set_xlim([ηBug[1,1],ηBug[end,1]+0.05])
-ax.set_ylim([0,max(maximum(ηBug[2:end,2]),maximum(η[2:end,2]),maximum(ηBound[2:end,2]),maximum(ηBugBound[2:end,2]))+1])
-ax.set_xlabel("time", fontsize=30);
-ax.set_ylabel(L"\eta", fontsize=30);
-ax.tick_params("both",labelsize=30);
-ax.legend(loc="upper right", fontsize=30);
-fig.canvas.draw() # Update the figure
-PyPlot.savefig("results/eta_planesource_theta$(ϑCoarse)_nx$(s.Nx)_N$(s.nPN).png")
-
-# plot η fine in time
 fig = figure("eta fine",figsize=(15,12),dpi=100)
 ax = gca()
 ax.plot(ηBugF[:,1],ηBugF[:,2], "r-.", label=L"BUG, $\bar\vartheta =$ "*LaTeXString(string(ϑFine)), linewidth=2, alpha=1.0)
